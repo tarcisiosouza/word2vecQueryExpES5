e547f9350edaf9ba317732b4f6527f97d73d5edd
tarcisio <tarcisio@ip35.236.mip.uni-hannover.de>
2018-05-31 13:46:52 -0300

    k

--- src/main/java/de/l3s/souza/word2vecseedfinder/QueryExpansion.java
+++ src/main/java/de/l3s/souza/word2vecseedfinder/QueryExpansion.java
@@ -426,6 +426,8 @@ public class QueryExpansion {
 		String uri = "http://eventkginterface.l3s.uni-hannover.de/sparql?default-graph-uri=&query="
 				+ URLEncoder.encode(query, "UTF-8") + "&format=json";
 
+		System.out.println (query);
+		
 		String lines = readLines(uri);
 		JSONObject json = new JSONObject(lines);
 
